6a234545745dffdff136e6278f446aa7beaafced
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2023-10-10 22:44:48 +0100

    Implemented Enums

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
 export default function Home() {
-  function getName(name: String) {
-    return name;
+  function getGpa(notes: number[]): number {
+    let gpa : number = 0;
+    notes.forEach(note => gpa += note)
+    return gpa / notes.length;
   }
-  return <div>{getName('Ilyas')}</div>;
+  enum ilyas {
+    nom = 'Ilyas El Mabrouki',
+    age = 21,
+    gpa = getGpa([15, 14, 16]),
+  }
+  return (
+    <div>{`Hi, my name is ${ilyas.nom} and I'm ${ilyas.age} and I got a gpa of ${ilyas.gpa}`}</div>
+  );
 }
