3ed0717620f2c2a302ec34e19cfa303bb5fcfc47
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2023-10-11 21:36:09 +0100

    Implemented interface

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
 export default function Home() {
-  function getGpa(notes: number[]): number {
-    let gpa : number = 0;
-    notes.forEach(note => gpa += note)
-    return gpa / notes.length;
+  interface person {
+    name: string;
+    age: number;
   }
-  enum ilyas {
-    nom = 'Ilyas El Mabrouki',
-    age = 21,
-    gpa = getGpa([15, 14, 16]),
+  function getData(ilyas: person) {
+    return `${ilyas.name} --> ${ilyas.age}`;
   }
-  return (
-    <div>{`Hi, my name is ${ilyas.nom} and I'm ${ilyas.age} and I got a gpa of ${ilyas.gpa}`}</div>
-  );
+  return <div>{getData({ name: 'ilyas', age: 20 })}</div>;
 }
